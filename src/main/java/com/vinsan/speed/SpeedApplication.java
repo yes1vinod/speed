10dcd1492fd245501f717cdf5d4f2140d1e4fad4
Vinod Santhanam <yes_vinod@yahoo.com>
2020-05-25 13:25:46 -0400

    first commit

--- src/main/java/com/vinsan/speed/SpeedApplication.java
+++ src/main/java/com/vinsan/speed/SpeedApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpeedApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpeedApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedApplication.class, args);
+    }
 
 }
